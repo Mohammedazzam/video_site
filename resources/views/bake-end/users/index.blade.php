9372f47e102b7dc780ce68713661bd7154fcdb47
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-02-02 21:50:36 +0200

    15.2(عملت انهانسمينت لل view وأنشأت مجلد جديد وملف جديد خاص بال table)

--- resources/views/bake-end/users/index.blade.php
+++ resources/views/bake-end/users/index.blade.php
@@ -17,83 +17,74 @@
 
     @endcomponent
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header card-header-primary">
-                    <div class="row">
 
-                        <div class="col-md-8">
-                            <h4 class="card-title ">{{$pageTitle}}</h4>
-                            <p class="card-category"> {{$pageDes}} </p>
-                        </div>
+    @component('bake-end.shared.table',['pageTitle' =>$pageTitle,'pageDes' =>$pageDes]) <!--هيك قمت بتمرير الباراميتر-->
 
-                         <div class="col-md-4 text-right" >
-                             <a href="{{route('users.create')}}" class="btn btn-white btn-round">
-                                 Add {{$sModuleName}}
-                             </a>
-                         </div>
-                    </div>
-
-
-                </div>
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <table class="table">
-                            <thead class=" text-primary">
-                            <tr><th>
-                                    ID
-                                </th>
-                                <th>
-                                    Name
-                                </th>
-
-                                <th>
-                                    email
-                                </th>
-                                <th class="text-right">
-                                    control
-                                </th>
-                            </tr></thead>
-                            <tbody>
-
-                                @foreach($rows as $row)
-                                    <tr>
-                                        <td>
-                                            {{$row->id}}
-                                        </td>
-                                        <td>
-                                            {{$row->name}}
-                                        </td>
-                                        <td>
-                                            {{$row->email}}
-                                        </td>
-                                        <td class="td-actions text-right">
-                                            <a href="{{route('users.edit', ['id'=> $row])}}" rel="tooltip" title="" class="btn btn-white btn-link btn-sm" data-original-title="Edit {{$sModuleName}}" aria-describedby="tooltip814720">
-                                                <i class="material-icons">edit</i>
-                                            </a>
-
-                                            <form action="{{route('users.destroy',['id'=> $row])}}" method="post">
-                                                {{csrf_field()}}
-                                                    {{method_field('delete')}}
-                                                <button type="submit" rel="tooltip" title="" class="btn btn-white btn-link btn-sm" data-original-title="Remove {{$sModuleName}}">
-                                                    <i class="material-icons">close</i>
-                                                </button>
-
-                                            </form>
-
-                                        </td>
-                                    </tr>
-                                @endforeach
-
-                            </tbody>
-                        </table>
-                        {!! $rows->links() !!} <!--هذه خاصة لل pagination -->
-                    </div>
-                </div>
+        @slot('addButton')
+            <div class="col-md-4 text-right" >
+                <a href="{{route('users.create')}}" class="btn btn-white btn-round">
+                    Add {{$sModuleName}}
+                </a>
             </div>
-        </div>
+         @endslot
+
+
+    <div class="table-responsive">
+        <table class="table">
+            <thead class=" text-primary">
+            <tr><th>
+                    ID
+                </th>
+                <th>
+                    Name
+                </th>
+
+                <th>
+                    email
+                </th>
+                <th class="text-right">
+                    control
+                </th>
+            </tr></thead>
+            <tbody>
+
+            @foreach($rows as $row)
+                <tr>
+                    <td>
+                        {{$row->id}}
+                    </td>
+                    <td>
+                        {{$row->name}}
+                    </td>
+                    <td>
+                        {{$row->email}}
+                    </td>
+                    <td class="td-actions text-right">
+                        <a href="{{route('users.edit', ['id'=> $row])}}" rel="tooltip" title="" class="btn btn-white btn-link btn-sm" data-original-title="Edit {{$sModuleName}}" aria-describedby="tooltip814720">
+                            <i class="material-icons">edit</i>
+                        </a>
+
+                        <form action="{{route('users.destroy',['id'=> $row])}}" method="post">
+                            {{csrf_field()}}
+                            {{method_field('delete')}}
+                            <button type="submit" rel="tooltip" title="" class="btn btn-white btn-link btn-sm" data-original-title="Remove {{$sModuleName}}">
+                                <i class="material-icons">close</i>
+                            </button>
+
+                        </form>
+
+                    </td>
+                </tr>
+            @endforeach
+
+            </tbody>
+        </table>
+    {!! $rows->links() !!} <!--هذه خاصة لل pagination -->
     </div>
+
+    @endcomponent
+
+
 
 @endsection
 
