f0ec30c8a1a1bb4a4baa057518189f9ae9bd0b54
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-02-09 17:45:38 +0200

    30.03( ترتيب الفورم لإظهار الكومنت والوقت والتاريخ لكل كومينت)

--- resources/views/bake-end/comments/index.blade.php
+++ resources/views/bake-end/comments/index.blade.php
@@ -1,3 +1,24 @@
-@foreach($comments as $comment)
-    {{$comment->comment}} <br>
-@endforeach
+<table class="table">
+
+    <tbody>
+
+    @foreach($comments as $comment)
+        <tr>
+            <td>
+                <p>{{$comment->comment}}</p>
+                <small>{{$comment->created_at}}</small>
+            </td>
+            <td class="td-actions text-right">
+                <button type="button" rel="tooltip" title="" class="btn btn-white btn-link btn-sm" data-original-title="Edit Task">
+                    <i class="material-icons">edit</i>
+                </button>
+                <button type="button" rel="tooltip" title="" class="btn btn-white btn-link btn-sm" data-original-title="Remove">
+                    <i class="material-icons">close</i>
+                </button>
+            </td>
+        </tr>
+    @endforeach
+
+    </tbody>
+
+</table>
