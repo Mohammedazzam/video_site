5904e2885eea633b2b27d603700b9ea9f4b76aec
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-02-10 01:12:46 +0200

    32(عمل تحسين للكود إخفاء الزر )

--- resources/views/bake-end/comments/index.blade.php
+++ resources/views/bake-end/comments/index.blade.php
@@ -10,16 +10,18 @@
                 <small>{{$comment->created_at}}</small>
             </td>
             <td class="td-actions text-right">
-                <button type="button" rel="tooltip" title="" class="btn btn-white btn-link btn-sm" data-original-title="Edit Task">
+                <button type="button" rel="tooltip" title="" class="btn btn-white btn-link btn-sm"
+                        data-original-title="Edit comment">
                     <i class="material-icons">edit</i>
                 </button>
-                <a href="{{route('comment.delete' , ['id' =>$comment->id])}}" rel="tooltip" class="btn btn-white btn-link btn-sm" data-original-title="Remove">
+                <a href="{{route('comment.delete' , ['id' =>$comment->id])}}" rel="tooltip" class="btn btn-white btn-link btn-sm"
+                   data-original-title="Remove comment">
                     <i class="material-icons">close</i>
                 </a>
             </td>
         </tr>
 
-        <tr>
+        <tr style="display: none">
             <td colspan="4">
                 <form action="{{route('comment.update', ['id' => $comment->id])}}" method="post">
                     {{csrf_field()}}
