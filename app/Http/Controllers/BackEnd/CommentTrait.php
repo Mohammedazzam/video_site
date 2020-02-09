da4631b0ce89ee89273e7a5f4e4365e74e7688f3
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-02-10 01:28:22 +0200

    32(عمل تحسين للكود ضفت  id عشان ينزلي على المكان الي بصير فيه التعديل)

--- app/Http/Controllers/BackEnd/CommentTrait.php
+++ app/Http/Controllers/BackEnd/CommentTrait.php
@@ -11,19 +11,19 @@ trait commentTrait{
         $requestArray = $request->all()+["user_id" =>auth()->user()->id];
 //        dd($requestArray);
         Comments::create($requestArray);
-        return redirect()->route('videos.edit',['id'=>$requestArray['video_id']]);
+        return redirect()->route('videos.edit',['id'=>$requestArray['video_id'],'#comments']);
     }
 
 
     public function commentDelete($id){
         $row = Comments::findOrFail($id);
         $row->delete($id);
-        return redirect()->route('videos.edit',['id'=>$row->video_id]);
+        return redirect()->route('videos.edit',['id'=>$row->video_id ,'#comments']);
     }
 
     public function commentUpdate($id, Store $request){
         $row = Comments::findOrFail($id);
         $row->update($request->all());
-        return redirect()->route('videos.edit',['id'=>$row->video_id]);
+        return redirect()->route('videos.edit',['id'=>$row->video_id , '#comments']);
     }
 }
