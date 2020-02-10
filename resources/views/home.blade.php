36b517c75d395c17b31d7704d002a24b23b52d7f
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-02-10 23:00:42 +0200

    35.3(ترتيب للصفحة ووضع كارد)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="section section-buttons">
+        <div class="container">
+            <div class="title">
+                <h2>Latest Videos</h2>
             </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="card" style="width: 20rem;">
+                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4">
+                    <div class="card" style="width: 20rem;">
+                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4">
+                    <div class="card" style="width: 20rem;">
+                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
         </div>
     </div>
-</div>
+
+
 @endsection
