eac046218b2e41f707689007e711b2996ef1ec6c
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-02-18 00:37:28 +0200

    93.3(لو واحد مسجل دخول ومش آدمين خليه يروح على ال Home)

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -9,15 +9,20 @@ class Admin
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        dd(auth()->user());
-        if (auth()->guest())
+//        dd(auth()->user());
+        if (auth()->guest()) {
             return redirect('/login');
+        }
+        if (auth()->user()->group != 'admin'){
+            return redirect('/');
+    }
         return $next($request);
+
     }
 }
