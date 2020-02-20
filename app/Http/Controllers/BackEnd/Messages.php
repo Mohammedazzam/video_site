b63f6426a02a50a1a2e9ae1ff85345adf8fa8c18
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-02-20 15:59:08 +0200

    42.2(عمل الفورم والبدء في عمل الكونترولر لل Mail)

--- app/Http/Controllers/BackEnd/Messages.php
+++ app/Http/Controllers/BackEnd/Messages.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 
+use App\Http\Requests\BackEnd\Messages\Store;
 use App\Models\Message;
 
 class Messages extends BackEndController
@@ -10,5 +11,9 @@ class Messages extends BackEndController
     public function __construct(Message $model)
     {
         parent::__construct($model);
+    }
+    public function replay($id , Store $request){
+        $message = $this->model->findOrFail($id);
+//        dd($request->message,$id);
     }
 }
--- app/Mail/ReplayContact.php
+++ app/Mail/ReplayContact.php
@@ -28,6 +28,6 @@ class ReplayContact extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('bake-end.mails.replay-message');
     }
 }
