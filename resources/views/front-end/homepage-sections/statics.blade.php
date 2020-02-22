5c3ac204f3d744bff98bda6c8bf1eca2ce96e6f3
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-02-22 22:17:59 +0200

    45(عمل بعض التنسيقات للصفحة)

--- resources/views/front-end/homepage-sections/statics.blade.php
+++ resources/views/front-end/homepage-sections/statics.blade.php
@@ -3,11 +3,11 @@
         <h2 class="title">Our Numbers</h2>
         <div class="row">
             <div class="col-md-4">
-                <div class="card card-profile card-plain">
+                <div class="card card-plain">
                     <div class="card-body">
                         <a href="#paper-kit">
                             <div class="author">
-                                <h2 class="card-title">{{$videos_count}}</h2>
+                                <h2 class="card-title"style="font-size: 50px">{{$videos_count}}</h2>
                                 <h4 class="card-category">Videos</h4>
                             </div>
                         </a>
@@ -15,11 +15,11 @@
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card card-profile card-plain">
+                <div class="card card-plain">
                     <div class="card-body">
                         <a href="#paper-kit">
                             <div class="author">
-                                <h2 class="card-title">{{$comments_count}}</h2>
+                                <h2 class="card-title"style="font-size: 50px">{{$comments_count}}</h2>
                                 <h4 class="card-category">Comments</h4>
                             </div>
                         </a>
@@ -27,11 +27,11 @@
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card card-profile card-plain">
+                <div class="card card-plain">
                     <div class="card-body">
                         <a href="#paper-kit">
                             <div class="author">
-                                <h2 class="card-title">{{$tags_count}}</h2>
+                                <h2 class="card-title "style="font-size: 50px">{{$tags_count}}</h2>
                                 <h4 class="card-category">Tags</h4>
                             </div>
                         </a>
