0e1922482ea88f8ff617023b0bd22a5b747e09a6
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-02-22 23:40:13 +0200

    46.2(اظهار الاسم الذي كنت أعمل عليه سيرش وعمل ال Reset رابط للرجوع )

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,11 @@
         <div class="container">
             <div class="title">
                 <h2>Latest Videos</h2>
+                @if(request()->has('search')&& request()->get('search')!='')
+                <p style="margin-top: 5px">
+                    You are Search On <b>{{request()->get('search')}}</b> | <a href="{{route('home')}}">Reset</a>
+                </p>
+                @endif
             </div>
             @include('front-end.shared.video-row')
         </div>
