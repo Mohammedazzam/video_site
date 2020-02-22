f50403338d7aa5807455743b075b20dd97192787
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-02-22 15:43:26 +0200

    44.4(عمل عدد المشاهدات والفيدوهات والاتاجات وعملهم في الكونترولر )

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -90,6 +90,9 @@ class HomeController extends Controller
 
     public function welcome(){
         $videos = Video::orderBy('id','desc')->paginate(9);
-        return view('welcome',compact('videos'));
+        $videos_count = Video::count(); //لإظهار عدد الفيديوهات
+        $comments_count = Comments::count();//لإظهار عدد الكومينتات
+        $tags_count = Tag::count();//لإظهار عدد التاجات
+        return view('welcome',compact('videos','videos_count','comments_count','tags_count'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
 @section('content')
     @include('front-end.homepage-sections.home-image')
     @include('front-end.homepage-sections.videos')
+    @include('front-end.homepage-sections.statics')
     @include('front-end.homepage-sections.contact-us')
 @endsection
 
