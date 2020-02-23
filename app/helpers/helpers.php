bd233feae8bf80ed4060057670b9f7bec3da1e8c
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-02-23 23:34:46 +0200

    47.2(عمل جزئية ال slug في ال helper function حتى تعمم)

--- app/helpers/helpers.php
+++ app/helpers/helpers.php
@@ -9,3 +9,8 @@ function getYoutubeId($url)
     preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $url, $match);
     return isset($match[1]) ? $match[1] : null;
 }
+
+
+function slug(string $name){
+   return strtolower(trim(str_replace(' ','_',$name))); //strtolower هذه عشان أصغر الحروف وأخليها سمول بدل كابيتل
+}
