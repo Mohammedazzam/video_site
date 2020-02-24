85feb7413b572f7328c43aadce277bbb87ca00fe
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-02-24 18:15:12 +0200

    50(ترتيب بعض الأيكون في الموقع)

--- resources/views/bake-end/home-sections/statics.blade.php
+++ resources/views/bake-end/home-sections/statics.blade.php
@@ -3,14 +3,13 @@
         <div class="card card-stats">
             <div class="card-header card-header-warning card-header-icon">
                 <div class="card-icon">
-                    <i class="material-icons">content_copy</i>
+                    <i class="fa fa-youtube"></i>
                 </div>
                 <p class="card-category">Videos</p>
                 <h3 class="card-title">{{\App\Models\Video::count()}}</h3>
             </div>
             <div class="card-footer">
                 <div class="stats">
-                    <i class="material-icons text-warning">warning</i>
                     <a href="{{route('videos.index')}}" class="warning-link">All Videos</a>
                 </div>
             </div>
@@ -20,7 +19,7 @@
         <div class="card card-stats">
             <div class="card-header card-header-success card-header-icon">
                 <div class="card-icon">
-                    <i class="material-icons">store</i>
+                    <i class="fa fa-book"></i>
                 </div>
                 <p class="card-category">Skills</p>
                 <h3 class="card-title">{{\App\Models\Skill::count()}}</h3>
@@ -36,7 +35,7 @@
         <div class="card card-stats">
             <div class="card-header card-header-danger card-header-icon">
                 <div class="card-icon">
-                    <i class="material-icons">info_outline</i>
+                    <i class="fa fa-tags"></i>
                 </div>
                 <p class="card-category">Tags </p>
                 <h3 class="card-title">{{\App\Models\Tag::count()}}</h3>
@@ -52,7 +51,7 @@
         <div class="card card-stats">
             <div class="card-header card-header-info card-header-icon">
                 <div class="card-icon">
-                    <i class="fa fa-twitter"></i>
+                    <i class="fa fa-comments"></i>
                 </div>
                 <p class="card-category">Comments</p>
                 <h3 class="card-title">{{\App\Models\Comments::count()}}</h3>
